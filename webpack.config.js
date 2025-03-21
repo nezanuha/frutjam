c0fc11e36dbfa6ef8f8592697ac8b8f1dfd095fa
Mohammed Rashad <rashadmehtab7@gmail.com>
2025-03-21 11:49:35 +0400

    Redesigned button styles with a modern look and added animations. Introduced new button styles: btn-dashed and btn-outline.

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,6 @@ module.exports = (env, argv) => {
                 directory: path.resolve(__dirname, 'dist'), // Serve content from 'dist' directory
             },
             compress: true, // Enable gzip compression
-            port: 9000, // Set the port for the dev server
             open: true, // Automatically open the browser
             hot: true, // Enable hot module replacement
             historyApiFallback: true, // Support for single-page applications
